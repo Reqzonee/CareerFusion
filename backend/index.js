20c95b5cd2012edf8b8011bdc2f470412982a025
paraj <bhattparaj2@gmail.com>
2024-08-08 21:30:35 +0530

    fix cors errors

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOption = {
-    origin: `${FRONTEND_URL}`,
+    origin: `${process.env.FRONTEND_URL}`,
     credentials:true 
 }
 app.use(cors(corsOption));
