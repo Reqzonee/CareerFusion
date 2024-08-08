8638f551a31749ffb52ed5cb64b66152855dfc5c
paraj <bhattparaj2@gmail.com>
2024-08-08 20:58:55 +0530

    update nodemon version

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,7 @@
-export const USER_API_END_POINT = "http://localhost:8000/api/v1/user";
-export const JOB_API_END_POINT = "http://localhost:8000/api/v1/job";
-export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
-export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+const apiUrl = import.meta.env.VITE_BACKEND_URL;
+
+
+export const USER_API_END_POINT =`${apiUrl}/user`;
+export const JOB_API_END_POINT = `${apiUrl}/job`;
+export const APPLICATION_API_END_POINT= `${apiUrl}/application`;
+export const COMPANY_API_END_POINT=`${apiUrl}/company`;
